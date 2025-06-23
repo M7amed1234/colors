397b0cebe457b334fd6e5c53633147a34d8a1688
M7amed1234 <mahamedalking323@gmail.com>
2025-06-23 15:41:18 +0200

    color

---
+++ script.js
@@ -0,0 +1,49 @@
+function color1(){
+
+    
+        document.getElementById("change-color1").addEventListener("click" ,
+        document.body.style.background="#ff0000"
+        
+        
+        )
+}
+
+function color2(){
+
+    
+        document.getElementById("change-color2").addEventListener("click" ,
+        document.body.style.background="#ffff00"
+        
+        
+        )
+}
+
+function color3(){
+
+    
+        document.getElementById("change-color3").addEventListener("click" ,
+        document.body.style.background="#0000ff"
+        
+        
+        )
+}
+
+function color4(){
+
+    
+        document.getElementById("change-color4").addEventListener("click" ,
+        document.body.style.background="#ffffff"
+        
+        
+        )
+}
+
+function color5(){
+
+    
+        document.getElementById("change-color5").addEventListener("click" ,
+        document.body.style.background="#000000"
+        
+        
+        )
+}
